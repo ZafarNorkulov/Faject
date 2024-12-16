ddcf7883595f5bd11f50fba291227822c451a56c
ZafarNorkulov <norqulovzafar123@gmail.com>
2024-12-16 23:24:17 +0500

    a

--- components/ModalMenu.tsx
+++ components/ModalMenu.tsx
@@ -39,7 +39,7 @@ const ModalMenu = ({
     (async () => {
       try {
         const response = await api.get("/category");
-        setData(response.data);
+        setData(response?.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
