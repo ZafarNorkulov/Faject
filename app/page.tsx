b567043b8f8cc8d248f1f71fecdac0eb0d513b41
ZafarNorkulov <norqulovzafar123@gmail.com>
2024-12-17 12:33:25 +0500

    add7

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import CookiePopUp from "@/components/CookiePopUp";
 import Faqs from "@/components/homepage/Faqs";
 import Header from "@/components/homepage/Header";
 import News from "@/components/homepage/News";
-import Projects from "@/components/homepage/Projects";
 import Tools from "@/components/homepage/Tools";
 import Result from "@/components/Result";
 import AOS from "aos";
@@ -19,7 +18,7 @@ export default function Home() {
   return (
     <div className="size-full overflow-x-hidden">
       <Header />
-      <Projects number={5} />
+      {/* <Projects number={5} /> */}
       {/* <Services /> */}
       <Tools />
       {/* <ContactForm /> */}
