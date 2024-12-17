73a3b76967a9d18286bf050b4eea51dc4f39b447
ZafarNorkulov <norqulovzafar123@gmail.com>
2024-12-17 12:25:09 +0500

    add3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import React from "react";
 import MainLayout from "@/components/MainLayout";
 import "./globals.css";
-import React from "react";
 
 export default function RootLayout({
   children,
